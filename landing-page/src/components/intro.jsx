028de4a91b8065e466a89d6a8dc90ee82b3b10ab
Guillermo Muñoz <guillemunozi2003@gmail.com>
2024-12-28 19:12:23 -0500

    Summary

---
+++ landing-page/src/components/intro.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import '../assets/Intro.css'
+
+export const Intro = () => {
+  return (
+    <nav>
+        <div> div
+            <p></p>
+        </div>
+        <ul>
+            <li><a href="#">1</a></li>
+            <li><a href="#">2</a></li>
+            <li><a href="#">3</a></li>
+            <li><a href="#">4</a></li>
+        </ul>
+    </nav>
+  )
+}
